9777061ea48415a5a72eb30964101cc734d7c109
Paweł Skalski <40362366+pawelskalski@users.noreply.github.com>
2019-10-19 13:35:52 +0200

    First commit

---
+++ CzwarteZajecia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CzwarteZajecia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] table= {1,2,3,4,5};
+            int[] newTable= {1,2,3,4,5};
+            foreach (var VARIABLE in table)
+            {
+                Console.Write(VARIABLE);
+            }
+            foreach (var VARIABLE in newTable)
+            {
+                Console.Write(VARIABLE);
+            }
+        }
+    }
+}
